d7a54b9018ffbb9329e145f0512204118d34d3d3
Jaden Brower <browerjaden7@gmail.com>
2024-09-04 10:57:47 +0300

    edited js

--- js/viesti.js
+++ js/viesti.js
@@ -1,12 +1,14 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm').addEventListener('submit', function (event) {
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
-    
+
+
     if (!email.includes('@')) {
         alert('Anna voimassa oleva sähköpostiosoite.');
         event.preventDefault();
         return;
     }
+
 
     if (message.trim() === '') {
         alert('Kirjoita viesti.');
